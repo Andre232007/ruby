e850a436631aba9e498d026cdb4b59088b6d69d6
Andre232007 <165347419+Andre232007@users.noreply.github.com>
2025-09-16 08:35:09 -0300

    Corrigir assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: './',
-  basePath: '/ruby',
+  basePath: '/ruby', // Apenas esta linha é necessária
   images: {
     unoptimized: true,
   },
